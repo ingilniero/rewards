ef200084ea13b1cbab2436ead7fa1fe8d2c49035
Gilberto Villa <ingilniero@gmail.com>
2018-09-06 11:46:56 -0500

    Complete users' current reward once it reaches 4 pieces

--- app/models/rewards/piece.rb
+++ app/models/rewards/piece.rb
@@ -2,10 +2,27 @@ module Rewards
   class Piece < ApplicationRecord
     belongs_to :reward
     belongs_to :bestowment
+    has_many :reward_pieces, through: :reward, source: :pieces
 
     enum identifier: %i[first_quarter
                         second_quarter
                         third_quarter
-                        foruth_quarter]
+                        fourth_quarter]
+
+    before_save :set_identifier
+
+    after_create :complete_reward
+
+    def set_identifier
+      self.identifier = next_piece
+    end
+
+    def next_piece
+      Piece.identifiers.key(reward_pieces.size)
+    end
+
+    def complete_reward
+      reward.complete! if fourth_quarter?
+    end
   end
 end
--- test/system/app_flows_test.rb
+++ test/system/app_flows_test.rb
@@ -16,7 +16,30 @@ class AppFlowsTest < ApplicationSystemTestCase
     sign_in angel
     visit '/rewards'
 
-    assert_text 'incomplete'
+    assert_text 'State: incomplete'
     assert_text 'first_quarter'
+  end
+
+  test "rewards" do
+    aranza = users(:aranza)
+    haro = users(:haro)
+
+    sign_in aranza
+    visit '/rewards'
+
+    click_on 'New Bestowment'
+    select 'Haro', from: 'bestowment_bestowed_id'
+    click_on 'Create Bestowment'
+
+    assert_selector 'p', text: 'Bestowment created successfully!'
+
+    sign_in haro
+
+    visit '/rewards'
+
+    assert_text 'first_quarter'
+    assert_text 'second_quarter'
+    assert_text 'third_quarter'
+    assert_text 'State: complete'
   end
 end
